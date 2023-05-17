a582890a5fcca83ab59ba795a50ae81689e1314f
Pedro Luis Alvarado Herrera <129191234+pedroalvaradoh@users.noreply.github.com>
2023-05-17 19:12:56 +0000

    feat: rectangle area calculator added

--- rectanglearea.js
+++ rectanglearea.js
@@ -1,1 +1,12 @@
+// // ## Problema 1: Calcular el área de un rectángulo
+// Escribe una función de JavaScript llamada computeArea que tome dos parámetros, ancho y alto, y devuelva el área del rectángulo.
+// javascript
+// // Ejemplo de uso:
+// const area = computeArea(5, 8)
+// console.log(area) // Resultado: 40
 
+
+const computeArea = (a,b) => {return a * b; }
+
+let area = computeArea(5, 8);
+console.log(area);
